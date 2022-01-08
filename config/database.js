b28137d4afb5df936f0eb8f96c9501c61b68c619
waelarous <waelarous2009@gmail.com>
2022-01-08 18:55:22 +0100

    ds

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-let db = mongoose.connect('mongodb://localhost:27017/eventsDB',{ useNewUrlParser: true } , (err)=> {
-
+let db = mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0.io0m2.mongodb.net/DSframework?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  (err) => {
     if (err) {
-        console.log(err)
+      console.log(err);
     } else {
-        console.log('connected to db succcesfuly...')
+      console.log("connected to db succcesfuly...");
     }
-})
+  }
+);
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,87 +1,88 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
-const passport = require('passport')
-const multer = require("multer")
-// configure multer 
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const passport = require("passport");
+const multer = require("multer");
+// configure multer
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/images')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + '.png') 
-    }
-  })
-  
-  var upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, "uploads/images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + ".png");
+  },
+});
+
+var upload = multer({ storage: storage });
 // middleware to check if user is loogged in
 
-isAuthenticated = (req,res,next) => {
-    if (req.isAuthenticated()) return next()
-    res.redirect('/users/login')
-}
-//  login user view 
-router.get('/login', (req,res)=> {
-    res.render('user/login', {
-        error: req.flash('error')
-    })
-})
+isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/users/login");
+};
+//  login user view
+router.get("/login", (req, res) => {
+  res.render("user/login", {
+    error: req.flash("error"),
+    pagename: "login",
+  });
+});
 
-// login post request 
-router.post('/login',
-  passport.authenticate('local.login', {
-    successRedirect: '/users/profile',
-      failureRedirect: '/users/login',
-      failureFlash: true })
-      )
+// login post request
+router.post(
+  "/login",
+  passport.authenticate("local.login", {
+    successRedirect: "/users/profile",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })
+);
 
-
-// sign up form 
-router.get('/signup', (req,res)=> {
-    res.render('user/signup', {
-        error: req.flash('error')
-    })
-})
+// sign up form
+router.get("/signup", (req, res) => {
+  res.render("user/signup", {
+    error: req.flash("error"),
+    pagename: "signup",
+  });
+});
 
 // sign up post request
 
-router.post('/signup',
-  passport.authenticate('local.signup', {
-    successRedirect: '/users/profile',
-      failureRedirect: '/users/signup',
-      failureFlash: true })
-      )
+router.post(
+  "/signup",
+  passport.authenticate("local.signup", {
+    successRedirect: "/users/profile",
+    failureRedirect: "/users/signup",
+    failureFlash: true,
+  })
+);
 
-// progile 
-router.get('/profile',isAuthenticated, (req,res)=> {
-
-res.render('user/profile', {
-    success: req.flash('success')
-})
-  
-
-})
+// profile
+router.get("/profile", isAuthenticated, (req, res) => {
+  res.render("user/profile", {
+    success: req.flash("success"),
+    pagename: "profile",
+  });
+});
 
 //upload user avatar
 
-router.post('/uploadAvatar', upload.single('avatar'), (req,res)=> {
-    
-    let newFields = {
-        avatar: req.file.filename
+router.post("/uploadAvatar", upload.single("avatar"), (req, res) => {
+  let newFields = {
+    avatar: req.file.filename,
+  };
+  User.updateOne({ _id: req.user._id }, newFields, (err) => {
+    if (!err) {
+      res.redirect("/users/profile");
     }
-    User.updateOne( {_id: req.user._id}, newFields, (err)=> {
-        if (!err) {
-            res.redirect('/users/profile')
-        }
-
-    } )
-})
+  });
+});
 
 // logout user
 
-router.get('/logout', (req,res)=> {
-    req.logout();
-    res.redirect('/users/login');
-})
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/users/login");
+});
 
-module.exports = router
+module.exports = router;
